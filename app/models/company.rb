1f043f25d6f2d19ee2db36c1f296b027478a4eb8
dutt23 <shatyaki_dutt@hotmail.com>
2020-07-18 22:53:56 +0530

    add model level validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,10 @@
 class Company < ApplicationRecord
   has_rich_text :description
 
+  validates_format_of :email, :allow_blank => true,:with => /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/, :message => "is invalid, please validate the domain and format"
+
+  validates_format_of :phone, length: {minimum: 10, maximum: 10}, :allow_blank => true,:with => /(?:\+?|\b)[0-9]{10}\b/, :message => "is invalid, please validate the entered number"
+
   def save
 
     if self.zip_code.present?
@@ -9,7 +13,7 @@ class Company < ApplicationRecord
         throw_error("Please validate your entered zip code")
         return false
       end
-      self.location_id = zip_code_id
+        self.location_id = zip_code_id
       super
     else
       throw_error("Please check your zip code")
@@ -17,7 +21,7 @@ class Company < ApplicationRecord
  
   end
 
-  
+
   private 
   def throw_error(message)
     self.errors.add(:Error, message)
