ecf15c3b48a174153d2f6e4a3724cfb8caa4c170
dutt23 <shatyaki_dutt@hotmail.com>
2020-07-18 20:51:16 +0530

    Add back end validations and error messages

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,12 @@
 class CompaniesController < ApplicationController
   before_action :set_company, except: [:index, :create, :new]
 
+  rescue_from RailsParam::Param::InvalidParameterError do | exception |
+   @company = Company.new(company_params)
+    @company.errors[:base] << exception.message
+   render :new 
+   end
+
   def index
     @companies = Company.all
   end
@@ -13,6 +19,13 @@ class CompaniesController < ApplicationController
   end
 
   def create
+    param! :company, Hash, required: true do |record, index|
+      record.param! :name, String, required: true, message: "Company name required"
+      record.param! :zip_code, String, required: true, message: "Zip code required"
+      record.param! :description, String
+      record.param! :phone, String, format: /(?:\+?|\b)[0-9]{10}\b/, message: "Please check your phone number"
+      record.param! :email, String, format: /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/, message: "Please check your email"
+    end
     @company = Company.new(company_params)
     if @company.save
       redirect_to companies_path, notice: "Saved"
@@ -49,5 +62,5 @@ class CompaniesController < ApplicationController
   def set_company
     @company = Company.find(params[:id])
   end
-  
+
 end
