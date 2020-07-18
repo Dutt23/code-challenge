28df4a35b04831f3bcfa9d50dcc5added1c923c2
dutt23 <shatyaki_dutt@hotmail.com>
2020-07-18 23:53:42 +0530

    update compnay

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,24 +6,40 @@ class Company < ApplicationRecord
   validates_format_of :phone, length: {minimum: 10, maximum: 10}, :allow_blank => true,:with => /(?:\+?|\b)[0-9]{10}\b/, :message => "is invalid, please validate the entered number"
 
   def save
-
-    if self.zip_code.present?
-      zip_code_id = ZipCodeService.new(self.zip_code).save_company_zip_data
-      if zip_code_id == 0
-        throw_error("Please validate your entered zip code")
-        return false
-      end
-        self.location_id = zip_code_id
-      super
+    if update_zip_code
+        super
     else
-      throw_error("Please check your zip code")
+      return false
     end
- 
+  end
+
+  def update(company_params)
+    if update_zip_code
+        super
+    else
+      return false
+    end
   end
 
 
   private 
-  def throw_error(message)
+
+  def update_zip_code
+    if self.zip_code.present?
+      zip_code_id = ZipCodeService.new(self.zip_code).save_company_zip_data
+      if zip_code_id == 0
+        populate_errors("Please validate your entered zip code")
+        return false
+      end
+      self.location_id = zip_code_id
+      return true
+    else
+      populate_errors("Please check your zip code")
+      return false
+    end
+  end
+
+  def populate_errors(message)
     self.errors.add(:Error, message)
   end
 
